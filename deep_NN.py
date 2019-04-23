fe174daa134546fc9170d28e3b533a5c726142a3
Vigneswaran <120003362@sastra.ac.in>
2019-04-23 16:33:24 +0530

    Mutual Info and Shannon Entropy added

--- deep_NN.py
+++ deep_NN.py
@@ -1,11 +1,12 @@
 """
-This file contains Simple Feed forward Neural Network from scratch implemented in Pytorch
+Simple Feed forward Neural Network from scratch implemented in Pytorch with various easy-use and helpful features
 """
 import matplotlib.pyplot as plt
 from random import randint
 from torch.utils.data import DataLoader, Dataset
+import torch.nn.functional as fun
 import torch
-import numpy as numpy
+import numpy as np
 import pandas as pd
 import torch.nn as nn
 import warnings
@@ -21,11 +22,11 @@ class FocalvsNonFocalData(Dataset):
     def __init__(self, csv_file, root_dir, transform=None):
         xf = pd.read_csv(csv_file['Focal'], header = 0)
         xf.columns = [str(i) for i in range(xf.shape[1])]
-        xf[str(xf.shape[1])] = numpy.zeros(xf.shape[0])
+        xf[str(xf.shape[1])] = np.zeros(xf.shape[0])
 
         xn = pd.read_csv(csv_file['NFocal'], header = 0)
         xn.columns = [str(i) for i in range(xn.shape[1])]
-        xn[str(xn.shape[1])] = numpy.ones(xn.shape[0])
+        xn[str(xn.shape[1])] = np.ones(xn.shape[0])
 
         self.datasamples = xf.append(xn)
         self.root_dir = root_dir
@@ -36,7 +37,11 @@ class FocalvsNonFocalData(Dataset):
     
     def __getitem__(self,index):
         sample = self.datasamples.iloc[index,:-1]
-        return(sample.as_matrix())
+        target = self.datasamples.iloc[index,-1]
+        return(sample.as_matrix(), target, index)
+
+    def return_dimension(self):
+        return(self.datasamples.shape[1])
 
     def plotsample(self, index):
         sample = self.datasamples.iloc[index,:-1].as_matrix()
@@ -45,69 +50,94 @@ class FocalvsNonFocalData(Dataset):
         plt.ylabel('mV')
         plt.show()
 
+    def return_statistics(self, index, prop = 'all'):
+        sample = np.array(self.datasamples.iloc[index,:-1].as_matrix())
+        mean = sample.mean()
+        std = sample.std()
+        var = sample.var()
+        minn = sample.min()
+        maxx = sample.max()
+        if prop == 'all':
+            print(" == Statistical values of the sample == ")
+            print("Mean: %f Standard Dev: %f Varience: %f Maximum: %f Minimum: %f", mean, std, var, minn, maxx)
+            print(" ====================================== ")
+        else:
+            stat_dict = {'mean':mean, 'std': std, 'var':var, 'min':minn, 'max':maxx}
+            if prop in list(stat_dict.keys()):
+                print(prop, stat_dict[prop])
+            else:
+                raise KeyError("The given key is not available. Make sure you used the correct key. Available keys: 'all','mean','std','var','min','max'")
+
+    def visualize_dataset(self, start_index, end_index, criterion = 'all', label = 'all'):
+        """
+        Implement various clustering plots to visualize the dataset with respect to various staistical properties
+        :TODO 
+        """
+        
+class deepNeuralNet(nn.Module):
+    
+    def __init__(self, input_dim, nh1, nh2, nh3, nh4, out_dim):
+        #initialize model parameters and hyperparameters
+        super(deepNeuralNet,self).__init__()
+        self.input_dim = input_dim
+        self.nh1, self.nh2, self.nh3, self.nh4 = nh1, nh2, nh3, nh4
+        
+        #initalize layers
+        self.linear1 = nn.Linear(input_dim, nh1)
+        self.linear2 = nn.Linear(nh1, nh2)
+        self.linear3 = nn.Linear(nh2, nh3)
+        self.linear4 = nn.Linear(nh3, nh4)
+        self.linear5 = nn.Linear(nh4, out_dim)
+
+    def forward(self, x):
+        #propogate through each layer
+        self.out1 = fun.relu(self.linear1(x))
+        self.out2 = fun.relu(self.linear2(self.out1))
+        self.out3 = fun.relu(self.linear3(self.out2))
+        self.out4 = fun.relu(self.linear4(self.out3))
+        self.out5 = fun.sigmoid(self.linear5(self.out4))
+        return(self.out5)
+
+def calc_MI(X,Y,bins):
+
+   """
+   Should employ accurate methods for calculating MI
+   refer: https://openreview.net/forum?id=ry_WPG-A-
+   """
+   c_XY = np.histogram2d(X,Y,bins)[0]
+   c_X = np.histogram(X,bins)[0]
+   c_Y = np.histogram(Y,bins)[0]
+
+   H_X = shan_entropy(c_X)
+   H_Y = shan_entropy(c_Y)
+   H_XY = shan_entropy(c_XY)
+
+   MI = H_X + H_Y - H_XY
+   return MI
+
+def shan_entropy(c):
+    c_normalized = c / float(np.sum(c))
+    c_normalized = c_normalized[np.nonzero(c_normalized)]
+    H = -sum(c_normalized* np.log2(c_normalized))  
+    return H
+
+bins = 20 # ?  #should add function to calculate optimal number of bins by analyzing the activation of neurons. 
+
+for ix in np.arange(n):
+    for jx in np.arange(ix+1,n):
+        matMI[ix,jx] = calc_MI(A[:,ix], A[:,jx], bins)
 data_set = FocalvsNonFocalData({'Focal':'Focal.csv', 'NFocal':'NFocal.csv'}, 'data/')
 
-dataloader = DataLoader(data_set, batch_size=128, shuffle=True)
+dataloader = DataLoader(data_set, batch_size=128, shuffle=True, num_workers=4)
 
-class Neural_Network(nn.Module):
-    def __init__(self, ):
-        super(Neural_Network, self).__init__()
-        # parameters
-        # TODO: parameters can be parameterized instead of declaring them here
-        self.inputSize = 2
-        self.outputSize = 1
-        self.hiddenSize = 3
-        
-        # weights
-        self.W1 = torch.randn(self.inputSize, self.hiddenSize) # 3 X 2 tensor
-        self.W2 = torch.randn(self.hiddenSize, self.outputSize) # 3 X 1 tensor
-        
-    def forward(self, X):
-        self.z = torch.matmul(X, self.W1) # 3 X 3 ".dot" does not broadcast in PyTorch
-        self.z2 = self.sigmoid(self.z) # activation function
-        self.z3 = torch.matmul(self.z2, self.W2)
-        o = self.sigmoid(self.z3) # final activation function
-        return o
-        
-    def sigmoid(self, s):
-        return 1 / (1 + torch.exp(-s))
-    
-    def sigmoidPrime(self, s):
-        # derivative of sigmoid
-        return s * (1 - s)
-    
-    def backward(self, X, y, o):
-        self.o_error = y - o # error in output
-        self.o_delta = self.o_error * self.sigmoidPrime(o) # derivative of sig to error
-        self.z2_error = torch.matmul(self.o_delta, torch.t(self.W2))
-        self.z2_delta = self.z2_error * self.sigmoidPrime(self.z2)
-        self.W1 += torch.matmul(torch.t(X), self.z2_delta)
-        self.W2 += torch.matmul(torch.t(self.z2), self.o_delta)
-        
-    def train(self, X, y):
-        # forward + backward pass for training
-        o = self.forward(X)
-        self.backward(X, y, o)
-        
-    def saveWeights(self, model):
-        # we will use the PyTorch internal storage functions
-        torch.save(model, "NN")
-        # you can reload model with all the weights and so forth with:
-        # torch.load("NN")
-        
-    def predict(self):
-        print ("Predicted data based on trained weights: ")
-        print ("Input (scaled): \n" + str(xPredicted))
-        print ("Output: \n" + str(self.forward(xPredicted)))
-
-criteron = nn.MSELoss()
-
-optimizer = torch.optim.SGD(model.parameters(), lr = 0.01)
+model = deepNeuralNet(37, 50, 70, 100, 10, 1)
+epochs = 100
+criterion = nn.MSELoss()
+print(model.parameters)
+optimizer = torch.optim.SGD(model.parameters, lr=0.001)
 
 for epoch in range(epochs):
-    y_pred = model(x)
-    loss = criteron(y_pred, y)
-    print("Epoch: ",epoch, "Loss: ",loss.item())
-    optimizer.zero_grad()
-    loss.backward()
-    optimizer.step()
+    for x, y, ind in dataloader:
+        y_pred = model(x)
+        loss = criterion(y_pred, y)
+        print(loss)
