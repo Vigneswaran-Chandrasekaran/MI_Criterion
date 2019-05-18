7511138f6808cbdb17e1a301ce3b2661e74728f9
Vigneswaran <120003362@sastra.ac.in>
2019-05-18 13:47:09 +0530

    Added full NN

--- minst_nn.py
+++ minst_nn.py
@@ -1,20 +1,23 @@
+from torch.utils import data
 from torch.utils.data import DataLoader
-from mutual_infor import mutual_information_2d
 import torchvision.transforms as transforms
 import torchvision.datasets as dataset
 import torch.nn.functional as fun
+import matplotlib.pyplot as plt
 import torch.nn as nn
 import numpy as np
 import torchvision
 import torch
 
-train_dataset = dataset.MNIST(root = './data', train = True, transform = transforms.ToTensor(), download = True )
+master_dataset = dataset.MNIST(root = './data', train = True, transform = transforms.ToTensor(), download = True )
 test_dataset = dataset.MNIST(root = './data', train = False, transform = transforms.ToTensor())
+train_dataset, val_dataset = data.random_split(master_dataset, (int(len(master_dataset)*0.8), int(len(master_dataset)*0.2)))
 
-batch_size = 6000
-
-train_loader = DataLoader(dataset = train_dataset, batch_size = batch_size, shuffle = True)
-test_loader = DataLoader(dataset = test_dataset, batch_size = batch_size, shuffle = False)
+tr_batch_size = 4800
+val_batch_size = 1200
+train_loader = DataLoader(dataset = train_dataset, batch_size = tr_batch_size, shuffle = True)
+val_loader = DataLoader(dataset = val_dataset, batch_size = val_batch_size, shuffle = True)
+test_loader = DataLoader(dataset = test_dataset, batch_size = tr_batch_size, shuffle = False)
 
 class DeepNN(nn.Module):
 
@@ -42,43 +45,82 @@ class DeepNN(nn.Module):
         self.out4 = fun.relu(self.hlayer3(self.out3))
         self.out5 = fun.relu(self.hlayer4(self.out4))
         self.out6 = fun.relu(self.output_layer(self.out5))
-        return fun.log_softmax(self.out6, dim = 1)
+        return fun.softmax(self.out6, dim = 1)
 
     def return_layer_output(self):
+
         return self.out1, self.out2, self.out3, self.out4, self.out5, self.out6
 
-obj = DeepNN(784, 1000, 1200, 1100, 1000, 100, 10)
+model = DeepNN(784, 1000, 1200, 1100, 1000, 100, 10)
+criterion = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(model.parameters(), lr = 0.001)
 
-#criterion = nn.CCELoss()
-optimizer = torch.optim.Adam(obj.parameters(), lr = 0.001)
-
-epochs = 5
+epochs = 2
+step_size = 10
 train_loss = []
-mutual_info_monitor = []
+train_acc = []
+val_loss =[]
+val_acc = []
 
 for epoch in range(epochs):
 
-    loss_monitor = []
-    mutual_info = 0
+    epoch_loss = 0
+    epoch_acc = 0
+    epoch_val_loss = 0
+    epoch_val_acc = 0
+
     for i,(images, labels) in enumerate(train_loader):
 
         images = images.reshape(-1, 28*28)
         optimizer.zero_grad()
-        pred = obj(images)
-        
-        a, b, c, d, e, f = obj.return_layer_output()
-
-        mutual_info += mutual_information_2d(a,b,c,d,e,f,labels)
-
-        loss = fun.nll_loss(pred, labels)
+        output = model(images)
+        loss = criterion(output, labels)
+        epoch_loss += loss.item()
         loss.backward()
         optimizer.step()
-
-        if (i+1) % 5 == 0:                              
-            loss_monitor.append(loss.item())
-            print('Epoch [%d/%d], Step [%d/%d], Loss: %.4f'%(epoch+1, epochs, i+1, len(train_dataset)//batch_size, loss.item()))
-
-    print('Epoch %d/%d Loss: %.5f'%(epoch+1,epochs,sum(loss_monitor)/len(loss_monitor)))
-    train_loss.append(sum(loss_monitor)/len(loss_monitor))
-    mutual_info_monitor.append(mutual_info)
+        _, pred = torch.max(output.data, 1)
+        epoch_acc += float((pred == labels.data).sum())
+    
+    tr_acc = epoch_acc/len(train_dataset)
+    tr_loss = epoch_loss/len(train_loader)
+    train_loss.append(tr_loss)
+    train_acc.append(tr_acc)
+    
+    for j, (images, labels) in enumerate(val_loader):
+    
+        images = images.reshape(-1, 28*28)
+        output = model(images)
+        loss = criterion(output, labels)
+        epoch_val_loss += loss.item()
+        _, pred = torch.max(output.data, 1)
+        epoch_val_acc += float((pred == labels.data).sum())
+    
+    v_acc = epoch_val_acc/len(val_dataset)
+    v_loss = epoch_val_loss/len(val_loader)
+    val_loss.append(v_loss)
+    val_acc.append(v_acc)
+    
+    print('Epoch %d/%d : ' %(epoch+1,epochs))
+    print('Train loss: %.5f, Train acc: %.5f' %(tr_loss, tr_acc))    
+    print('Validation loss: %.5f, Validation acc: %.5f' %(v_loss, v_acc))    
     print(" --------------------- ")
+
+plt.plot(range(1, epochs+1), val_loss, train_loss)
+plt.title('Loss monitor')
+plt.legend(['Val_loss', 'Train_loss'])
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.show()
+
+plt.plot(range(1, epochs+1), val_acc, train_acc)
+plt.title('Accuracy monitor')
+plt.legend(['Val_acc', 'Train_acc'])
+plt.xlabel('Epochs')
+plt.ylabel('Accuracy')
+plt.show()
+
+full_state = { 'epoch':epochs, 'state_dict':model.state_dict(), 'optimizer':optimizer.state_dict(), 
+            'train_loss':train_loss, 'train_acc': train_acc,
+            'val_loss':val_loss, 'val_acc':val_acc}
+            
+torch.save(full_state, 'SaveModel')
