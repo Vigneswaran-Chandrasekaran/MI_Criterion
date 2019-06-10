72db0631c2b604cd40e99f97a157627348c39df5
Vigneswaran <120003362@sastra.ac.in>
2019-06-10 22:57:17 +0530

    Added freezing feature

--- main.py
+++ main.py
@@ -13,20 +13,25 @@ import time
 
 @profile
 def dataset_load(tr_batch_size, val_batch_size, val_split):
+    
     #Download and prepare dataset chunks by DataLoader
     tic = time.time()
     print("Downloading dataset and preparing DataLoader")
     master_dataset = dataset.MNIST(root = './data', train = True, transform = transforms.ToTensor(), download = True )
     test_dataset = dataset.MNIST(root = './data', train = False, transform = transforms.ToTensor())
+    
     #Train and validation data is split with specified ratio ratio
     train_dataset, val_dataset = data.random_split(master_dataset, (int(len(master_dataset)*(1.0-val_split)), int(len(master_dataset)*val_split)))
+    
     #define dataloaders with defined batch size for training and validation
     train_loader = DataLoader(dataset = train_dataset, batch_size = tr_batch_size, shuffle = True)
     val_loader = DataLoader(dataset = val_dataset, batch_size = val_batch_size, shuffle = True)
     test_loader = DataLoader(dataset = test_dataset, batch_size = tr_batch_size, shuffle = False)
     toc = time.time()
+    
     print("Finished preparing. Total time elasped: "+str(toc - tic)+" seconds")
     print("Memory profile for dataset_load(): ")
+    
     return( train_loader, val_loader, test_loader)
 
 class DeepNN(nn.Module):
@@ -34,7 +39,9 @@ class DeepNN(nn.Module):
     Class defining the structure of Deep Neural Network model and layers characterstics
     """
     def __init__(self, input_dim, nh1, nh2, nh3, nh4, nh5, output_dim):
+        
         super(DeepNN, self).__init__()
+        
         #hyperparameter setting 
         self.input_dim = input_dim
         self.nh1, self.nh2, self.nh3, self.nh4, self.nh5, = nh1, nh2, nh3, nh4, nh5
@@ -48,6 +55,7 @@ class DeepNN(nn.Module):
         self.output_layer = nn.Linear(self.nh5, self.output_dim)
         
     def forward(self, x):
+
         #propogation of each layer
         self.out1 = fun.softmax(self.input_layer(x))
         self.out2 = fun.softmax(self.hlayer1(self.out1))
@@ -55,34 +63,86 @@ class DeepNN(nn.Module):
         self.out4 = fun.softmax(self.hlayer3(self.out3))
         self.out5 = fun.softmax(self.hlayer4(self.out4))
         self.out6 = fun.softmax(self.output_layer(self.out5))
+        
         return fun.softmax(self.out6, dim = 1)
 
-def clustering_neurons(neuronal_MI, k):
-    clusters = [[]*i for i in range(k)]
+def stopping_criterion(iteration_count, clusters, bin_avg, neuronal_MI):
+    """
+    Check the current stage is sufficient to stop the iteration and 
+    move on to next layer
+    Criteria:
+    i) Calculate best bin average MI and assign it as best solution
+       and then assign average MI for bins. When weights are updated based on 
+       bin ranking, check how many overlaps have occured. 
+       If number of overlaps in each is equal or greater than degree of required overlapping,
+       then stop the freezing.
+    ii) If iteration_count > 50 (per se)
+    """
+    if iteration_count >= 50:
+        print("Iteration count reached maximum.. proceeding to next layer")
+        return True
+    
+    
 
-
-@profile
+#@profile
 def pre_train_model(model):
+    
     layers = [model.input_layer, model.hlayer1, model.hlayer2, model.hlayer3, model.hlayer4]
+    k_value = [10, 8, 5, 3, 2]
+    
     for l_indx in range(len(layers)):
+    
+        print("Working on layer: "+str(l_indx))
         w_matrix = layers[l_indx].weight.data.clone().detach().numpy()
         b_matrix = layers[l_indx].bias.data.clone().detach().numpy()
+    
         #load the input data X
-        """
-        for j, (images, labels) in enumerate(val_loader):
-            images = images.reshape(-1, 28*28).clone().detach().numpy()
-            activation = np.dot(images,w_matrix.T) + b_matrix 
-            activation = 1/(1 + np.exp(-activation))
-        """
-        neuronal_MI = np.random.rand(b_matrix.shape[0])
-        clusters = clustering_neurons(neuronal_MI, k_value[l_indx])
+        if l_indx == 0:
+            for j, (images, labels) in enumerate(val_loader):
+                images = images.reshape(-1, 28*28).clone().detach().numpy()
+                activation = np.dot(images,w_matrix.T) + b_matrix 
+                activation = 1/(1 + np.exp(-activation))
+            X_input = images
+            H_activation = activation
+    
+        print("Estimation Information Theorotic quantities")
+        #neuronal_MI = calculate_fitness(X_input, H_activation)    
+        neuronal_MI = np.random.rand(b_matrix.shape[0])          # get MI between X_input and H_activation
+        # Get the index of sorted neurons based on MI value
+        index_sorted = np.argsort(neuronal_MI)[::-1]
+        #create clusters of given k value
+        clusters = np.array_split(index_sorted, k_value[l_indx])
+
+        bin_avg = []
+        for i in clusters:
+            bin_avg.append(np.sum(neuronal_MI[np.ix_(i)]) / neuronal_MI[np.ix_(i)].shape[0])
+        
+        while not stopping_criterion(iteration_count, clusters, bin_avg, neuronal_MI):
+    
+            iteration_count = 0
+    
+            for bin in range(len(clusters)):
+    
+                step_size,  = calculate_step_size(clusters[bin]) 
+                cost_function = calculate_cost_function(bin, clusters)
+                w_matrix[np.ix_(clusters[bin])] -= step_size * cost_function
+                b_matrix[np.ix_(clusters[bin])] -= step_size * cost_function
+                iteration_count += 1
+
+            activation = get_activation(l_indx, w_matrix, b_matrix)
+            #neuronal_MI = calculate_fitness(X_input, H_activation)    
+
+        model.input_layer.weight.data = w_matrix
+        model.input_layer.weight.data = b_matrix
+    
     return(model)
 
 if __name__ == '__main__':        
+
     tr_batch_size = 4800
     val_batch_size = 12000
     val_split = 0.2
     train_loader, val_loader, test_loader = dataset_load(tr_batch_size, val_batch_size, val_split)
-    model = DeepNN(784, 1024, 1200, 1200, 1200, 200, 10)
+    model = DeepNN(784, 1024, 120, 20, 20, 20, 10)
     print("Pretraining phase..")
     pre_trained_model = pre_train_model(model)
--- mutual_info_estimation.py
+++ mutual_info_estimation.py
@@ -35,6 +35,17 @@ def joint_entropy(X, Y):
         sum += np.sum((y_i * p_X) * np.log(y_i * p_X))
     return(-1*sum)
 
+def conditional_entropy(X, Y):
+    summation = 0
+    for value_x in values_x:
+        for value_y in values_y:
+            px = shape(where(data[x_index]==value_x))[1] / self.n_cols
+            py = shape(where(data[y_index]==value_y))[1] / self.n_cols
+            pxy = len(where(in1d(where(data[x_index]==value_x)[0], 
+                            where(data[y_index]==value_y)[0])==True)[0]) / self.n_cols
+            if pxy > 0.0:
+    summation += pxy * math.log((pxy / (px*py)), log_base)
+
 def kullback_divergence(X_bin_size, Y_bin_size, X, Y):
     X_bin, Y_bin = make_histogram(X_bin_size, Y_bin_size, X, Y)
     return(joint_entropy(X_bin, Y_bin) - estimate_mutual_info(X_bin_size, Y_bin_size, X, Y))
