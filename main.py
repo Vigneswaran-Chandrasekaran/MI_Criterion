54801a01073d3cb675a50d584fd028925e211be9
Vigneswaran <120003362@sastra.ac.in>
2019-06-19 23:22:08 +0530

    Today Update

--- main.py
+++ main.py
@@ -11,6 +11,8 @@ import torchvision
 import torch
 import time
 import math
+import warnings
+warnings.filterwarnings("ignore")
 
 #@profile     # to see the memory profile of the function uncomment the  @profile decorator
 def dataset_load(tr_batch_size, val_batch_size, val_split):
@@ -76,45 +78,79 @@ def estimate_mutual_info(X, neurons, bins = 5):
     # return array of mutual information corresponding to each neuron
     return(neuronal_MI)
 
+def check_k_Helly(bin_avg_new, bin_avg, clusters_new, clusters, k_helly):
+    # Using k-helly property to check wheteher the stopping condition is satisfied or not
+            
+
 #@profile
 def pre_train_model(model, val_loader):
-    
+    # list of all model's hidden layers
     layers = [model.input_layer, model.hlayer1, model.hlayer2, model.hlayer3, model.hlayer4]
-    k_value = [10, 8, 5, 3, 2]
-    
+    # Hyperparameter k-value which determines number of bins for the particular layer
+    # This is calaculated based on Partial Information Decomposition 
+    k_value = [4, 8, 5, 3, 2]
+    # k-helly stopping criterion's k-value to chec degree of overlap
+    k_helly = [5, 5, 5, 5, 5]
+    # loop over the layers 
     for l_indx in range(len(layers)):
-    
+        # get the layer's parameters and detach it for updation 
         print("Working on layer: "+str(l_indx))
         w_matrix = layers[l_indx].weight.data.clone().detach().numpy()
         b_matrix = layers[l_indx].bias.data.clone().detach().numpy()
         #load the input data X
-        
+        # use the validation dataset images as X
         for _, (images, _) in enumerate(val_loader):
-
+            # reshape images 
             images = images.reshape(-1, 28*28).clone().detach().numpy()
+            # find the activaion by,
+            # act( X.W_T + b)
             activation = np.dot(images,w_matrix.T) + b_matrix 
             activation = 1/(1 + np.exp(-activation))
-
+            # Estimate Mutual Information for each neuron
             tic_est = time.time()
             print("Estimation Information Theorotic quantities")
             neuronal_MI = estimate_mutual_info(images, activation, bins = 2)
             toc_est = time.time()
-        
             print("Elasped time for estimation: "+str(round(toc_est-tic_est,1))+" seconds")
+            print("----- Remeber: this elasped time is commenstruate with time required for pre-training the particular layer -----")
             # Get the index of sorted neurons based on MI value
             index_sorted = np.argsort(neuronal_MI)[::-1]
             #create clusters of given k value
-
             clusters = np.array_split(index_sorted, k_value[l_indx])
+            # calculate the bin's avergage MI
             bin_avg = []
-            
             for i in clusters:
                 bin_avg.append(np.sum(neuronal_MI[np.ix_(i)]) / neuronal_MI[np.ix_(i)].shape[0])
-            
-            print(neuronal_MI)
-            print(bin_avg)
+            # start tuning parameters !!!
+            # hyperparameters defined 
+            iteration = 0
+            decay_factor = 100    # decay factor for step-size
+            stopping_criteria = True
+            while stopping_criteria:
+                print("Iteration: "+str(iteration))
+                for bin in range(len(clusters)):
+                    #TODO: parameter updation very important.... High priority    
+                    # w_matrix have n number of rows with each row representing particular neuron connection
+                    w_matrix[np.ix_(clusters[bin])] -=  0.0001
+                    b_matrix[np.ix_(clusters[bin])] -=  0.0001
+                #calculate activation for the updated weights 
+                activation = np.dot(images,w_matrix.T) + b_matrix 
+                activation = 1/(1 + np.exp(-activation))
+                #estimate MI for the tuned parameters
+                neuronal_MI = estimate_mutual_info(images, activation, bins = 2)
+                index_sorted = np.argsort(neuronal_MI)[::-1]
+                #create new clusters from the estimated weight
+                for neuron_mi in neuronal_MI:
+                    if neuron_mi >= bin_avg[0]:
+                        
+
+                iteration += 1
+                if iteration == 3:
+                    stopping_criteria = False
+            layers[l_indx].weight.data = w_matrix
+            layers[l_indx].bias.data = b_matrix
+
             exit(1)
-        
     return(model)
 
 class DeepNN(nn.Module):
@@ -148,8 +184,6 @@ class DeepNN(nn.Module):
         self.out6 = fun.softmax(self.output_layer(self.out5))
         
         return fun.softmax(self.out6, dim = 1)
-
-    
 
 
 if __name__ == '__main__':        
