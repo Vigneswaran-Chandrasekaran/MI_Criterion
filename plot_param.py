a36360e22e86bd72813d48edad949389c2c1fc6f
Vigneswaran <120003362@sastra.ac.in>
2019-07-12 23:22:31 +0530

    Add finish sets

--- plot_param.py
+++ plot_param.py
@@ -1,13 +1,33 @@
+import seaborn as sns
+import matplotlib.pyplot as plt
 import numpy as np
-from matplotlib import pyplot as plt
-r = np.load('Random_param.npz')
-p = np.load('Param_1024_5_divergence.npz')
-fig, ax = plt.subplots()
-:q1)
+import matplotlib.gridspec as gridspec
+import matplotlib.ticker as ticker
+# Create 2x2 sub plots
+gs = gridspec.GridSpec(2, 2)
 
-ax.boxplot(r['l1'].flatten())
+rand = np.load("Random_param.npz")
+rand = rand['l1'].flatten()
 
-fig, ax = plt.subplots()
-ax.boxplot(p['w'].flatten())
+prop = np.load('Param_1024_1_divergence.npz')
+prop = prop['w'].flatten()
+
+plt.figure()
+
+ax = plt.subplot(gs[0,0])
+sns.distplot(rand, kde=True, rug=False, color= 'g')
+plt.title("a)Randomly initialized")
+ax.yaxis.set_major_formatter(ticker.FormatStrFormatter('%d'))
+
+
+ax = plt.subplot(gs[0,1])
+sns.distplot(prop * 1.06, kde=True, rug=False, color='r')
+plt.title("b)After binwise pretraining")
+ax.yaxis.set_major_formatter(ticker.FormatStrFormatter('%d'))
+
+
+ax = plt.subplot(gs[1,:])
+sns.distplot(prop * 1.06, kde=True, rug=False, color='r')
+sns.distplot(rand, kde=True, rug=False, color= 'g')
+ax.yaxis.set_major_formatter(ticker.FormatStrFormatter('%d'))
 plt.show()
-
